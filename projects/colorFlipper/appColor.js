61bac97c5b42fce140e81f3b36293af19d242154
Christiano0407 <placebo0704@gmail.com>
2021-09-14 16:07:53 -0500

    appcolor2

--- projects/colorFlipper/appColor.js
+++ projects/colorFlipper/appColor.js
@@ -1,3 +1,8 @@
 console.group("Color Project");
+// Variables >>>>
+
+// events and functions >>>>
+
+// llamados >>>>
 
 console.groupEnd();
---
+++ projects/colorFlipper/appHexJS.js
@@ -0,0 +1,8 @@
+console.group("HEX / HEXADECIMAL");
+// Variables >>>>
+
+// events and functions >>>>
+
+// llamados >>>>
+
+console.groupEnd();
