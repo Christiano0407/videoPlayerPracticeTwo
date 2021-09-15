54168bc7f2242160365713afee04c822f341f2db
Christiano0407 <placebo0704@gmail.com>
2021-09-15 09:31:29 -0500

    add colors numbers

--- projects/colorFlipper/appColor.js
+++ projects/colorFlipper/appColor.js
@@ -1,7 +1,35 @@
 console.group("Color Project");
 // Variables >>>>
+const colors = [
+  "green",
+  "red",
+  "rgba(133, 122, 200)",
+  "#f15025",
+  "#53A69D",
+  "#60BFAE",
+  "#F2DC9B",
+  "#F2AD85",
+];
+const btn = document.getElementById(`btn`);
+const color = document.querySelector(`.color`);
 
 // events and functions >>>>
+btn.addEventListener(`click`, function () {
+  //console.log(document.body);
+  // get random number between 0 - 3 colors[3];
+  const randomNumber = getRandomNumber(); //2; // posición del color>
+  console.log(randomNumber);
+  //body = completo>
+  document.body.style.backgroundColor = colors[randomNumber];
+  // texto = tomamos el nombre de la variable >
+  color.textContent = colors[randomNumber];
+});
+
+//> random number >
+// Math.floor => Traer el numero entero / Random => 0 a 1 / length => cantidad de elementos >>>>>>>>
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
 
 // llamados >>>>
 
