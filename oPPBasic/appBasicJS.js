52ae82f16ed48d68adfb60018dbc5f666a2b29cf
Christiano0407 <placebo0704@gmail.com>
2021-09-20 21:07:23 -0500

    prototype01

--- oPPBasic/appBasicJS.js
+++ oPPBasic/appBasicJS.js
@@ -43,4 +43,53 @@ console.log(teams);
 // Instance Of =>
 console.log(teams instanceof team); // True>
 
+// <<< ===============================================================>>>>>>
+// OwnProperties
+
+let girlTeam = function (name, number, city) {
+  this.name = name;
+  this.number = number;
+  this.city = city;
+};
+
+let girlsTeams = new girlTeam("The Power Pink", 12, "Las Vegas");
+console.log(girlsTeams);
+
+let ownProps = ["Almas"];
+
+for (let property in girlsTeams) {
+  if (girlsTeams.hasOwnProperty(property)) {
+    ownProps.push(property);
+  }
+}
+console.log(ownProps);
+
+// <<<<<<< ================ Use Prototype Properties to Reduce Duplicate Code =================== >>>>
+function gamer(name, age) {
+  this.name = name;
+  this.age = age;
+}
+gamer.prototype.nation = "Americana";
+
+let gamers = new gamer("Luisa", 21);
+console.log(gamers);
+console.log(gamers.nation);
+
+let ownsProps = [];
+let prototypeProps = [];
+
+for (let property in gamers) {
+  if (gamers.hasOwnProperty(property)) {
+    ownsProps.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+}
+console.log(ownsProps);
+console.log(Object.keys(ownsProps));
+console.log(prototypeProps);
+console.log(Object.keys(prototypeProps));
+
+// <<<============ Change the Prototype to a New Object ============================== >
+
 console.groupEnd();
