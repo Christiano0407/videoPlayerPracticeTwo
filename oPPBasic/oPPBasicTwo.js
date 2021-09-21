d308f559eaa6c0fcfca5828cb5826cd083e2498b
Christiano0407 <placebo0704@gmail.com>
2021-09-21 08:22:52 -0500

    class

--- oPPBasic/oPPBasicTwo.js
+++ oPPBasic/oPPBasicTwo.js
@@ -1,14 +1,52 @@
 console.group("Objects");
 // Object  ===>>>
-
-function Student(name, age, points) {
+// 1) ================================================================== >>>
+function Student(name, age, points, courseOk) {
   this.name = name;
   this.age = age;
   this.points = points;
+  this.courseOk = courseOk;
+  // methods One>
 }
+// Method Two for Prototype>>>>>
+Student.prototype.approvedCourses = function (newCourse) {
+  this.courseOk.push(newCourse);
+};
+
 //> Palabra reservada "NEW">>>
-const luisa = new Student("Luisa", 31, 750);
+const luisa = new Student("Luisa", 31, 750, "Course Basic Programming");
+const natalia = new Student(
+  "Natalia",
+  30,
+  2000,
+  "Course CSS GRID and Javascript"
+);
+
 // Estancia de Objeto => STUDENT>
 console.log(luisa);
+console.log(natalia);
+
+// 29 <<<<<<< ========================================================== >>>>>>>>>>>>>>>>>>>
+const liz = {
+  name: "Lizbeth",
+  age: 26,
+  cursosAprobados: [
+    "Curso Definitivo de HTML Y CSS",
+    "Curso Práctico de HTML y CSS",
+  ],
+  // crear un Method>>
+  aprobarCurso(nuevoCurso) {
+    this.cursosAprobados.push(nuevoCurso);
+  },
+};
+// Method > Agregar un curso
+//liz.cursosAprobados.push("Curso de Javascript");
+
+console.log(liz);
+liz.aprobarCurso("Curso de Javascript OPP");
+liz.aprobarCurso("Curso de CSS GRID");
+console.log(liz.cursosAprobados[0]);
+
+// 3) <<<<<<<<<<<< ======================================================================== >>>>>>>>>>>>>>>
 
 console.groupEnd();
---
+++ oPPBasic/oPPClassJsThree.js
@@ -0,0 +1,6 @@
+console.group("Class in Javascript");
+// >>>> Clases / class ======================== >>>>>
+// JS > Está basado en prototypes o prototipos >>>>>
+// Azúcar sintáctica ==== Una forma más fácil de hacer >>>>>>
+
+console.groupEnd();
