ddef68e68d9feb2dcb5869960d5a239403a84d0e
Christiano0407 <placebo0704@gmail.com>
2021-09-28 12:20:21 -0500

    Memoria Js

--- README.md
+++ README.md
@@ -43,6 +43,12 @@ Platform Projects with Javascript Vanilla.
 - Enumerable: Indica si la propiedad puede ser mostrada en la enumeración de las mismas. Existen ciertos métodos que toman como referencia este valor para mostrar la propiedad.
 - Writable: Esta indica si la propiedad puede ser modificada con el operador de asignación (=)
 - [defineProperty](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty)
+  > Seal
+- method seals an object, preventing new properties from being added to it and marking all existing properties as non-configurable. Values of present properties can still be changed as long as they are writable.
+- [seal](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/seal)
+  > Freze
+- ethod freezes an object. A frozen object can no longer be changed; freezing an object prevents new properties from being added to it, existing properties from being removed, prevents changing the enumerability, configurability, or writability of existing properties, and prevents the values of existing properties from being changed. In addition, freezing an object also prevents its prototype from being changed. freeze() returns the same object that was passed in.
+- [freeze](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze)
   > Symbol
 - [Symbol](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol)
 
--- oPPIntermedioJS/oPPDefinePropertyTwo.js
+++ oPPIntermedioJS/oPPDefinePropertyTwo.js
@@ -1,4 +1,69 @@
 console.group("Define Property");
 // < Con esta propiedad se pueden definir nuevas propiedades a nuestro objeto >
 
+const alma = {
+  name: "Alma",
+  age: 29,
+  aproveCourses: [],
+
+  // Method >
+  addCourse(newCourse) {
+    this.aproveCourses.push(newCourse);
+  },
+};
+console.log(alma);
+alma.addCourse("Course Javascript");
+console.log(Object.getOwnPropertyDescriptors(alma));
+
+// defineProperty >>>>>>>>>>
+// > Examples >
+// (1)
+Object.defineProperty(alma, "girl", {
+  value: "Girlfriend",
+  enumerable: true,
+  writable: true,
+  configurable: true,
+  // Puede ejecutar todo ====>>>>>>>>>>
+});
+
+// (2)
+Object.defineProperty(alma, "friend", {
+  value: "BestFriend",
+  enumerable: false, // No aparecera en la lista de valores e clave >>>
+  writable: true,
+  configurable: true,
+});
+
+// (3)
+Object.defineProperty(alma, "daughter", {
+  value: "Daughter Girl",
+  enumerable: true,
+  writable: false, // No se puede sobrescribir >>>
+  configurable: true,
+});
+
+// (4)
+Object.defineProperty(alma, "working", {
+  value: "CEO",
+  enumerable: true,
+  writable: true,
+  configurable: false, // No se puede borrar >>>
+});
+
+// << ====================================================================================================== >>
+// Recuerda que, ayuda al encapsulamiento >>
+console.log(alma);
+console.log(Object.keys(alma)); // La diferencia es que, no aparece "friend"
+console.log(Object.getOwnPropertyNames(alma)); // "A quí sí" > La diferencia con "keys()"
+
+// << =======================================================================>
+// (A) << Seal and Freeze>>
+// seal => Todas las propiedades en false> Se puedan borrar = configurable = false>>
+console.log(Object.seal(alma));
+
+// Freeze
+// writable = false> No se puede sobrescribir> / configurable = false / Ambos
+console.log(Object.freeze(alma));
+
+console.log(Object.getOwnPropertyDescriptors(alma));
 console.groupEnd();
