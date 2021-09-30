b3817280b388ec3c61719293986f0abc75b15daa
Christiano0407 <placebo0704@gmail.com>
2021-09-30 12:41:12 -0500

    modal previous 2

---
+++ projects/modal/appModal.js
@@ -0,0 +1,20 @@
+console.group("Modal | Window");
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+// >> ==================================================================>>
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(`.modal-overlay`);
+const closeBtn = document.querySelector(`.close-btn`);
+
+// OPEN ==>>
+modalBtn.addEventListener(`click`, function () {
+  modal.classList.add(`open-modal`);
+});
+// CLOSE ==>>
+closeBtn.addEventListener(`click`, function () {
+  modal.classList.remove(`open-modal`);
+});
+console.groupEnd();
