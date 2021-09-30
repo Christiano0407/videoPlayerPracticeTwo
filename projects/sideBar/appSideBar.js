47e1a4e0b6059f47856bccf6293c34ecc1770d6d
Christiano0407 <placebo0704@gmail.com>
2021-09-30 10:19:58 -0500

    sidebar three

--- projects/sideBar/appSideBar.js
+++ projects/sideBar/appSideBar.js
@@ -1,3 +1,30 @@
 console.group("Sidebar");
+const toggleBtn = document.querySelector(`.sidebar-toggle`);
+const closeBtn = document.querySelector(`.close-btn`);
+const sidebar = document.querySelector(`.sidebar`);
+
+//> Events ==>
+// (A) Open ==>
+toggleBtn.addEventListener(`click`, function () {
+  // (1)
+  if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove(`show-sidebar`);
+  } else {
+    sidebar.classList.add(`show-sidebar`);
+  }
+
+  // (2)
+  //sidebar.classList.toggle(`show-sidebar`);
+});
+
+// (B) ==> Close
+closeBtn.addEventListener(`click`, function () {
+  sidebar.classList.remove(`show-sidebar`);
+});
+
+//>
+console.log(toggleBtn);
+console.log(closeBtn);
+console.log(sidebar);
 
 console.groupEnd();
