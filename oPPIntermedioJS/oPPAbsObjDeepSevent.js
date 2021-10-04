7f2c4ad273bd2ab1a213ff03a54cb4fb595800bb
Christiano0407 <placebo0704@gmail.com>
2021-10-04 09:08:02 -0500

    valores

--- oPPIntermedioJS/oPPAbsObjDeepSevent.js
+++ oPPIntermedioJS/oPPAbsObjDeepSevent.js
@@ -1,3 +1,95 @@
 console.group("Abstracción con Objetos Literales y Deep Copy");
 
+// <<<<<<<<<< ================================================= >>>>>>>>>>>>
+// (A)
+// ===============================================================================>
+// (2)
+// Validar si es un Object>
+function isObject(subject) {
+  //typeOf = Tipo de valor>
+  return typeof subject == "object";
+}
+// Validar si es un Array>
+function isArray(subject) {
+  return Array.isArray(subject);
+}
+
+// (1) Deep Copy >>
+// subject = parameter, que vamos a validar>
+function deepCopy(subject) {
+  //Vamos a devolver >
+  let copySubject;
+
+  // (3) Validar Obj o Array >>
+  const subjectIsArray = isArray(subject);
+  const subjectIsObject = isObject(subject);
+
+  // (4) Revalidar / conditional >>
+  if (subjectIsArray) {
+    copySubject = [];
+  } else if (subjectIsObject) {
+    copySubject = {};
+  } else {
+    return subject;
+  }
+
+  // (5)
+  // Iteración y llamados recursivos >>
+  for (key in subject) {
+    const keyIsObject = isObject(subject[key]);
+    //(true)
+    if (keyIsObject) {
+      // Valor recursivo>
+      copySubject[key] = deepCopy(subject[key]);
+    } else {
+      if (subjectIsArray) {
+        copySubject.push(subject[key]);
+      } else {
+        copySubject[key] = subject[key];
+      }
+    }
+  }
+
+  return copySubject;
+}
+
+// Abstracción sin prototipos ==>
+// (1 / 6)
+const studentBase = {
+  name: undefined,
+  email: undefined,
+  age: undefined,
+  approvedCourses: undefined,
+  learningPaths: undefined,
+  socialMedia: {
+    twitter: undefined,
+    instagram: undefined,
+    facebook: undefined,
+  },
+};
+//==>>
+const luisa = deepCopy(studentBase);
+const rebeca = deepCopy(studentBase);
+// 1)
+//luisa["name"] = "Luisa";
+luisa.email = "Lui@gmail.com";
+luisa.socialMedia = { instagram: "@Luigi", twitter: "@Luisa91" };
+
+rebeca.name = "Rebeca Landa";
+
+//2)
+Object.defineProperty(luisa, "name", {
+  value: "Luisa Sánchez",
+  enumerable: true, //(Yes)
+  writable: false,
+  configurable: false, //(No)
+});
+// == Con seal ya no es necesario defineProperty ==
+//No lo podemos borrar / Ningúna propiedad se puede borrar =>
+Object.seal(rebeca);
+
+//>>> Call / Llamar >
+//console.log(studentBase);
+console.log(luisa);
+console.log(rebeca);
 console.groupEnd();
